67ca9b5148c29bb22dbbeb0ec16faee0eb502dcf
pistoolero <morwiko@gmail.com>
2017-09-27 20:11:46 +0200

    FB Login implementation

--- application/vendor/composer/autoload_real.php
+++ application/vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit5593553b82465ed94809828e0f24ecc5
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit5593553b82465ed94809828e0f24ecc5', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
